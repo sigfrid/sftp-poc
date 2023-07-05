39c0d391ba4707b1fbc0b36e879351120d8a4bf6
Sigfrid Dusci <sigfrid@users.noreply.github.com>
2023-07-05 16:56:04 +0200

    Update script.rb

--- script.rb
+++ script.rb
@@ -8,9 +8,8 @@ logger = Logger.new('log/logfile.log')
 listener = Listen.to(ENV['DIR_PATH']) do |_modified, added, _removed|
   unless added.empty?
     added.each do |path|
-      json_file = File.read(path)
-      control_plan = JSON.parse(json_file, object_class: OpenStruct).test
-      logger.info("Added file ID: #{control_plan.id} CODE: #{control_plan.pianoControllo} EDITION: #{control_plan.versionePiano} FILE LENGTH: #{json_file.each_line.count}")
+      control_plan = JSON.parse(File.read(path), object_class: OpenStruct).test
+      logger.info("ID: #{control_plan.id} CODE: #{control_plan.pianoControllo} EDITION: #{control_plan.versionePiano} COUNT: #{control_plan.dettagli.item.count}")
     end
   end
 end
